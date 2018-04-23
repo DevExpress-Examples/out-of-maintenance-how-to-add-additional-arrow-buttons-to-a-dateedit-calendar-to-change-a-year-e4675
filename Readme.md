ef8a26e027cc1fc928bce0c2be35bdf419ed5500
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,13 @@
+# How to add additional arrow buttons to a DateEdit calendar to change a year
+
+
+<p>This example illustrates how to create a custom DateEdit to show year arrow buttons near the month arrow buttons.</p>
+
+
+<h3>Description</h3>
+
+<p>Starting with version 15.2, DateEdit supports navigation by years. To do it, you need to set&nbsp;the&nbsp;<strong>RepositoryItemDateEdit.ShowYearNavigationButtons</strong> property to true.</p>
+
+<br/>
+
+
---
+++ VB/DXApplication1/Form1.vb
@@ -0,0 +1,19 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports System.Data
+Imports System.Drawing
+Imports System.Linq
+Imports System.Text
+Imports System.Windows.Forms
+
+Namespace DXApplication1
+	Partial Public Class Form1
+		Inherits DevExpress.XtraEditors.XtraForm
+
+		Public Sub New()
+			InitializeComponent()
+			dateEdit1.Properties.ShowYearNavigationButtons = DevExpress.Utils.DefaultBoolean.True
+		End Sub
+	End Class
+End Namespace
